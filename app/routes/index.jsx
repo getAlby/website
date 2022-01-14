00aaeaf10a6d2521a5581aa10725ffb3cedff6a4
Morrism1 <morrismontana0@gmail.com>
2022-01-14 03:49:07 +0200

    feature(header): load the header component

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,7 @@
+import Header from "~/components/Header";
+
 export default function Index() {
 	return (
-		<div className="grid place-items-center text-center h-screen">
-			<h1>Welcome to Remix</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-            15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-            Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-					</a>
-				</li>
-			</ul>
-		</div>
+		<Header />
 	);
 }
