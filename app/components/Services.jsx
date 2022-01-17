5ad8005c9a0d51d87cc967cc511ed7ca2f8d086a
ktscates <cates2506@gmail.com>
2022-01-17 12:37:51 +0200

    feature: add responsiveness

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -6,34 +6,34 @@ import Puzzle from "../../public/images/puzzle.png";
 export default function Services() {
     
     return(
-        <div className="w-full bg-albyYellow-50 px-37 pt-41 pb-38">
-            <div className="flex justify-center items-center space-x-21">
-                <div className="space-y-5">
+        <div className="xl:w-full py-12 bg-albyYellow-50 xl:px-37 xl:pt-41 xl:pb-38">
+            <div className="xl:flex xl:justify-center xl:items-center xl:space-x-21 space-y-5 xl:space-y-0">
+                <div className="space-y-2 block">
                     <div className="relative bg-albyYellow-200 rounded-full w-12.5 h-12.5 mx-auto">
-                        <img className="absolute top-2.5 w-13" src={Lines} alt="lines" />
+                        <img className="absolute top-2.5 xl:w-13" src={Lines} alt="lines" />
                     </div>
-                    <p className="text-2xl text-center">Use existing<br /> platforms</p>
+                    <p className="xl:text-2xl text-center">Use existing<br /> platforms</p>
                 </div>
                
-                <div className="space-y-5">
+                <div className="space-y-2 block">
                     <div className="relative bg-albyYellow-200 rounded-full w-12.5 h-12.5 mx-auto">
-                        <img className="absolute top-1.5 w-12" src={Wallet} alt="wallet" />
+                        <img className="absolute top-1.5 xl:w-12" src={Wallet} alt="wallet" />
                     </div>
-                    <p className="text-2xl text-center">Receive payments<br /> directly</p>
+                    <p className="xl:text-2xl text-center">Receive payments<br /> directly</p>
                 </div>
 
-                <div className="space-y-5">
-                    <div className="relative bg-albyYellow-200 rounded-full w-13.5 h-12.5 mx-auto mt-8">
-                        <img className="absolute top-1.5 w-14.5" src={Waves} alt="waves" />
+                <div className="space-y-2 block">
+                    <div className="relative bg-albyYellow-200 rounded-full w-13.5 h-12.5 mx-auto xl:mt-8">
+                        <img className="absolute top-1.5 xl:w-14.5" src={Waves} alt="waves" />
                     </div>
-                    <p className="text-2xl text-center">Non-intrusive and<br /> frictionless for your<br /> audience</p>
+                    <p className="xl:text-2xl text-center">Non-intrusive and<br /> frictionless for your<br /> audience</p>
                 </div>
 
-                <div className="space-y-5">
-                    <div className="relative bg-albyYellow-200 rounded-full w-12.5 h-12.5 mx-auto mt-8">
-                        <img className="absolute -mt-1.5 ml-1 w-11" src={Puzzle} alt="puzzle" />
+                <div className="space-y-2 block">
+                    <div className="relative bg-albyYellow-200 rounded-full w-12.5 h-12.5 mx-auto xl:mt-8">
+                        <img className="absolute -mt-1.5 ml-1 xl:w-11" src={Puzzle} alt="puzzle" />
                     </div>
-                    <p className="text-2xl text-center">Simple integration;<br /> no coding skills<br /> needed</p>
+                    <p className="xl:text-2xl text-center">Simple integration;<br /> no coding skills<br /> needed</p>
                 </div>
             </div>
         </div>    
