363f75c0962ea9b0195ba3e1846a0b4a2fe67286
ktscates <cates2506@gmail.com>
2022-01-17 11:41:37 +0200

    feature: create payment section

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,11 @@
 import Header from "~/components/Header";
+import Payment from "~/components/Payment";
 
 export default function Index() {
 	return (
 		<div>
 			<Header />
+			<Payment />
 		</div>
 	);
 }
