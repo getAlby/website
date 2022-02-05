45d0320ce318ee961ec141260785539b6191724e
Michael Bumann <hello@michaelbumann.com>
2022-02-05 17:43:00 +0100

    Add privacy policy

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -5,6 +5,7 @@ import TwitterLogo from "../../public/images/twitter.png";
 import GitHubLogo from "../../public/images/github.png";
 import DoItHint from "../../public/images/do-it-hint.png";
 import InstallExtensionButton from "../components/InstallExtensionButton";
+import Navigation from "../components/Navigation";
 
 export const meta = () => {
   return {
@@ -24,49 +25,7 @@ export default function index() {
     <>
       <div className=" bg-albyYellow-300 min-h-screen grid place-items-center relative">
         <div className="w-[93.194%] mx-auto ">
-          <div className="flex top-0 pt-5 pb-8 lg:pb-0 items-center absolute w-[93.194%] justify-center lg:justify-between max-h-[6.25rem]">
-            <div>
-              <a href="/">
-                <img
-                  src={Logo}
-                  alt="Alby"
-                  className="w-[12.5rem] text-black lg:w-auto lg:h-[3.75rem] h-24"
-                />
-              </a>
-            </div>
-            <div className="hidden lg:flex items-end gap-5 justify-between xl:pt-0 pt-3">
-              <a href="https://t.me/getAlby" target="_blank" rel="noreferrer">
-                <img
-                  src={TelegramLogo}
-                  alt="Telegram"
-                  className="w-[1.875rem] h-[1.875rem] mb-4"
-                />
-              </a>
-              <a
-                href="https://twitter.com/getalby"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={TwitterLogo}
-                  alt="Twitter"
-                  className="w-[1.875rem] h-[1.875rem]  mb-4"
-                />
-              </a>
-              <a
-                href="https://github.com/getAlby"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={GitHubLogo}
-                  alt="GitHub"
-                  className="w-[1.875rem] h-[1.875rem]  mb-4"
-                />
-              </a>
-              <InstallExtensionButton />
-            </div>
-          </div>
+          <Navigation />
           <div className="xl:mt-20 mt-22 lg:mt-0 flex flex-col lg:flex-row items-center lg:items-[inherit] justify-between font-secondary">
             <div className="xl:max-w-[39rem] lg:w-1/2 text-albyColdGray-800 text-center lg:text-left lg:pt-40">
               <h1 className="mb-4 lg:mb-0 xl:text-[4rem] xl:leading-[110%] text-black md:text-4xl text-3xl font-black">
@@ -165,7 +124,7 @@ export default function index() {
           </div>
 
           <a
-            href="https://getalby.com/privacy-policy"
+            href="/privacy-policy"
             className="text-center lg:text-2xl text-base underline pt-5"
           >
             Privacy Policy
