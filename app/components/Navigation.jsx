19cfdd289dc354f800dbe9d51612832a9514b3f4
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-02-10 17:14:18 -0700

    Remove socials from header, add Install Alby button, style login button

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -2,7 +2,7 @@ import Logo from "../../public/images/logo.svg";
 import TelegramLogo from "../../public/images/telegram.png";
 import TwitterLogo from "../../public/images/twitter.png";
 import GitHubLogo from "../../public/images/github.png";
-import InstallExtensionButton from "./InstallExtensionButton";
+import InstallExtensionButtonPlain from "./InstallExtensionButtonPlain";
 
 function Navigation() {
   return (
@@ -17,50 +17,20 @@ function Navigation() {
             />
           </a>
         </div>
-        <div className="flex items-end gap-5 justify-center md:justify-between xl:pt-0 pt-3">
+        <div className="flex flex-wrap items-end gap-5 justify-center md:justify-between xl:pt-0 pt-3">
           <a
             href="/value4value"
             class="border border-black rounded-full w-32 text-center py-2"
           >
-            Learn more
+            Value4Value
           </a>
           <a
             href="/login"
-            class="border border-black rounded-full w-32 text-center py-2"
+            class="bg-[#272828] text-white rounded-full w-32 text-center py-2"
           >
             Login
           </a>
-          <div class="hidden lg:flex items-end gap-5 justify-between">
-            <a href="https://t.me/getAlby" target="_blank" rel="noreferrer">
-              <img
-                src={TelegramLogo}
-                alt="Telegram"
-                className="w-[1.875rem] h-[1.875rem] mb-4"
-              />
-            </a>
-            <a
-              href="https://twitter.com/getalby"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={TwitterLogo}
-                alt="Twitter"
-                className="w-[1.875rem] h-[1.875rem]  mb-4"
-              />
-            </a>
-            <a
-              href="https://github.com/getAlby"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={GitHubLogo}
-                alt="GitHub"
-                className="w-[1.875rem] h-[1.875rem]  mb-4"
-              />
-            </a>
-          </div>
+          <InstallExtensionButtonPlain />
         </div>
       </div>
     </div>
