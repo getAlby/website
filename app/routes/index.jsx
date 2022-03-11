a1cdecdea951aaa26eb72efab22a16d0a8bb6b2c
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-03-10 21:44:12 -0700

    Dont show tweet section if tweets do not load

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import HeadLogo from "../../public/images/alby_icon_head_icon.svg";
 import BannerIllustration from "../../public/images/banner-illustration.svg";
 import DoItHint from "../../public/images/do-it-hint.png";
@@ -33,6 +34,7 @@ export const meta = () => {
 };
 
 export default function index() {
+  const [renderError, setRenderError] = useState(false);
   return (
     <>
       <div
@@ -209,29 +211,55 @@ export default function index() {
           </div>
         </div>
       </div>
+      {!renderError && (
+        <div className="bg-albyColdGray-800 py-32">
+          <h2 className="text-4xl font-extrabold text-white text-center mb-4">
+            What People Are Saying
+          </h2>
 
-      <div className="bg-albyColdGray-800 py-32">
-        <h2 className="text-4xl font-extrabold text-white text-center mb-4">
-          What People Are Saying
-        </h2>
-
-        <div className="block lg:grid grid-cols-3 gap-4 px-4 md:px-10 w-full">
-          <div>
-            <Tweet tweetId="1461415321823330305" options={{ theme: "dark" }} />
-            <Tweet tweetId="1482698978957271041" options={{ theme: "dark" }} />
-          </div>
-          <div>
-            <Tweet tweetId="1468991903349809152" options={{ theme: "dark" }} />
-            <Tweet tweetId="1485705677104369667" options={{ theme: "dark" }} />
-            <Tweet tweetId="1468036148844978181" options={{ theme: "dark" }} />
-          </div>
-          <div>
-            <Tweet tweetId="1483615302285807617" options={{ theme: "dark" }} />
-            <Tweet tweetId="1477623383454011392" options={{ theme: "dark" }} />
-            <Tweet tweetId="1477010314587561989" options={{ theme: "dark" }} />
+          <div className="block lg:grid grid-cols-3 gap-4 px-4 md:px-10 w-full">
+            <div>
+              <Tweet
+                tweetId="1461415321823330305"
+                options={{ theme: "dark" }}
+                renderError={() => setRenderError(true)}
+              />
+              <Tweet
+                tweetId="1482698978957271041"
+                options={{ theme: "dark" }}
+              />
+            </div>
+            <div>
+              <Tweet
+                tweetId="1468991903349809152"
+                options={{ theme: "dark" }}
+              />
+              <Tweet
+                tweetId="1485705677104369667"
+                options={{ theme: "dark" }}
+              />
+              <Tweet
+                tweetId="1468036148844978181"
+                options={{ theme: "dark" }}
+              />
+            </div>
+            <div>
+              <Tweet
+                tweetId="1483615302285807617"
+                options={{ theme: "dark" }}
+              />
+              <Tweet
+                tweetId="1477623383454011392"
+                options={{ theme: "dark" }}
+              />
+              <Tweet
+                tweetId="1477010314587561989"
+                options={{ theme: "dark" }}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      )}
 
       <div className="bg-albyYellow-300 relative py-32">
         <h2 className="text-4xl font-extrabold text-center mb-4">
