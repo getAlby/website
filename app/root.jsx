a19f1f8dc082d3d3a155c442ad7acaf70d4a1d16
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-03-15 12:40:59 -0600

    Run prettier

--- app/root.jsx
+++ app/root.jsx
@@ -16,7 +16,7 @@ export function links() {
     {
       rel: "icon",
       href: AlbyHeadIcon,
-      type: "image/png"
+      type: "image/png",
     },
     { rel: "stylesheet", href: styles },
     { rel: "stylesheet", href: extendStyles },
@@ -27,23 +27,45 @@ export default function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
-        <meta name="title" content="Alby — Lightning buzz for your Browser!"/>
-        <meta name="description" content="Alby brings Bitcoin to the web with in-browser payments and identity."/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://getalby.com/"/>
-        <meta property="og:title" content="Alby — Lightning buzz for your Browser!"/>
-        <meta property="og:description" content="Alby brings Bitcoin to the web with in-browser payments and identity."/>
-        <meta property="og:image" content={`https://getalby.com${OGImage}`}/>
-        <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content="https://getalby.com/"/>
-        <meta property="twitter:title" content="Alby — Lightning buzz for your Browser!"/>
-        <meta property="twitter:description" content="Alby brings Bitcoin to the web with in-browser payments and identity."/>
-        <meta property="twitter:image" content={`https://getalby.com${OGImage}`}/>
-	<meta name="twitter:site" content="@getalby"/>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="title" content="Alby — Lightning buzz for your Browser!" />
+        <meta
+          name="description"
+          content="Alby brings Bitcoin to the web with in-browser payments and identity."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://getalby.com/" />
+        <meta
+          property="og:title"
+          content="Alby — Lightning buzz for your Browser!"
+        />
+        <meta
+          property="og:description"
+          content="Alby brings Bitcoin to the web with in-browser payments and identity."
+        />
+        <meta property="og:image" content={`https://getalby.com${OGImage}`} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://getalby.com/" />
+        <meta
+          property="twitter:title"
+          content="Alby — Lightning buzz for your Browser!"
+        />
+        <meta
+          property="twitter:description"
+          content="Alby brings Bitcoin to the web with in-browser payments and identity."
+        />
+        <meta
+          property="twitter:image"
+          content={`https://getalby.com${OGImage}`}
+        />
+        <meta name="twitter:site" content="@getalby" />
 
-        <script defer data-domain="getalby.com" src="https://squirrel.getalby.com/js/plausible.js"></script>
+        <script
+          defer
+          data-domain="getalby.com"
+          src="https://squirrel.getalby.com/js/plausible.js"
+        ></script>
 
         <Meta />
         <Links />
