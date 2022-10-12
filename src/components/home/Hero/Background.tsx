ca2ed059fcf5a0c58033c12df3e8044ede4a88f1
Raj <varasaniraj5@gmail.com>
2022-10-12 23:51:28 +0530

    add responsive styles in home page hero component - add styles to update global font size for difference screen sizes

--- src/components/home/Hero/Background.tsx
+++ src/components/home/Hero/Background.tsx
@@ -17,18 +17,21 @@ const Background = () => {
         <Image
           src={BACKGROUND_DATA.images.coinFLip.src}
           alt={BACKGROUND_DATA.images.coinFLip.alt}
+          layout="fill"
         />
       </div>
       <div className={styles.coinFlipV2}>
         <Image
           src={BACKGROUND_DATA.images.coinFLipV2.src}
           alt={BACKGROUND_DATA.images.coinFLipV2.alt}
+          layout="fill"
         />
       </div>
       <div className={styles.polygon}>
         <Image
           src={BACKGROUND_DATA.images.polygon.src}
           alt={BACKGROUND_DATA.images.polygon.alt}
+          layout="fill"
         />
       </div>
     </div>
