ec2582b43496999357dcd5478702b9fce82c4c4f
Jan Kögel <i.jan.koegel@gmail.com>
2023-01-11 21:53:12 +0100

    prettier

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,10 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-	appDirectory: "app",
-	assetsBuildDirectory: "public/website",
-	publicPath: "/website/",
-	serverBuildDirectory: "build",
-	devServerPort: 8002,
-	ignoredRouteFiles: [".*"]
+  appDirectory: "app",
+  assetsBuildDirectory: "public/website",
+  publicPath: "/website/",
+  serverBuildDirectory: "build",
+  devServerPort: 8002,
+  ignoredRouteFiles: [".*"],
 };
